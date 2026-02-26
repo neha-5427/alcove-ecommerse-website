6d5542d75bc692fa1762213023cd4f244bc81065
Neha Rajesh <rane25jx@student.ju.se>
2026-02-26 16:58:34 +0100

    feat: cart page visible

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const cartGrid = document.querySelector(".cart-grid");
+
+let cartArr = JSON.parse(localStorage.cart);
+
+for (let i = 0; i < cartArr.length; i++) {
+  let cartBook = document.createElement("img");
+  cartBook.src = cartArr[i].img;
+  cartBook.style.width = "100%";
+  cartGrid.appendChild(cartBook);
+}
